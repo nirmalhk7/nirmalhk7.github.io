eaf452cb62c941d3c7b5c509852674aebb7c61bf
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-03-13 22:40:39 -0600

    Even more changes
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- src/components/Blog/blogIntroSection.tsx
+++ src/components/Blog/blogIntroSection.tsx
@@ -4,18 +4,18 @@ import WebSection from "@/elements/WebSection";
 
 const BlogIntroSection = ({ name }: { name: string }) => (
   <WebSection
-    className="bg-gradient-to-r from-accent/50 to-accent selection:bg-white selection:text-accent bg-white"
+    className="bg-gradient-to-r from-accent/50 to-accent selection:bg-white selection:text-accent bg-white text-white"
     id="blog"
   >
     <div className="narrow mx-auto text-center  pb-6 relative">
       <div className="w-full">
-        <h3 className="m-0 p-0 font-blocky  not-italic leading-tight text-white font-semibold mb-0 mt-0 uppercase">
+        <h3 className="leading-tight text-white my-0">
           {name}
         </h3>
-        <h1 className="mt-0 text-white">
+        <h1 className="mt-0">
           Latest From The Blog
         </h1>
-        <p className="text-white text-center m-0 p-0 font-normal">
+        <p className="text-center m-0 p-0 font-normal">
           I have strong views on topics like Finance, Technology, Future and
           Environment. Find me&nbsp;
           <Link className="text-white" title={name} href="/blog">
