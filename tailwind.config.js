cbe72758d2d40bf22ae4827204210ca21458dbd7
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-11-20 12:35:41 -0700

    fix(build fixes): fixed tailwind errors on screen width
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/**/*.{js,tsx,ts,jsx}"],
-  important: true,
   theme: {
     screens: {
       "mobile-l": { min: "576px" },
