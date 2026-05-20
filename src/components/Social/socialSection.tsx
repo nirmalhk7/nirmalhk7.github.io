294727d25ab74099272d238d7d0f140bfc3f0588
Nirmal Khedkar <nirmalhk7@gmail.com>
2026-05-19 23:16:48 -0600

    feat: add X social profile and enhance SocialSection with smooth hover animations

--- src/components/Social/socialSection.tsx
+++ src/components/Social/socialSection.tsx
@@ -3,6 +3,7 @@ import {
   faGoodreads,
   faLinkedin,
   faMastodon,
+  faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -20,6 +21,12 @@ export const social = [
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/nirmalhk7",
     class: faLinkedin,
+    underBlog: true,
+  },
+  {
+    name: "X",
+    link: "https://x.com/nirmalhk7",
+    class: faXTwitter,
     underBlog: true,
   },
   {
@@ -44,17 +51,23 @@ const SocialSection = () => {
   return (
     <ul className="pb-16 font-bold m-0 absolute bottom-0 right-16 font-blocky list-none after:block after:w-px after:bg-white after:bottom-0 after:content-[''] z-20">
       {social.map((element, index) => (
-        <li className="relative h-24" key={index}>
+        <li className="relative h-20 group" key={index}>
           <Magnetic>
             <button
-              className="button-white border-0 h-full text-3xl align-top"
+              className="border-0 h-full text-3xl align-top flex items-center justify-end text-white/70 hover:text-white transition-colors duration-300"
               onClick={() => {
                 trackClick(element.name, "social_link");
                 window.open(element.link, "_blank", "noopener,noreferrer");
               }}
               title={element.name}
             >
-              <FontAwesomeIcon icon={element.class} />
+              <span className="mr-6 text-xs uppercase tracking-[0.2em] opacity-0 -translate-x-4 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-0 pointer-events-none whitespace-nowrap text-accent">
+                {element.name}
+              </span>
+              <FontAwesomeIcon
+                icon={element.class}
+                className="transition-transform duration-300 group-hover:scale-125"
+              />
             </button>
           </Magnetic>
         </li>
